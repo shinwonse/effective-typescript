2128b373e394b85b8896e341b887f47125cf73d6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-10-12 19:19:10 +0900

    속성이 있는지 체크하여 타입을 명확하게 합니다

---
+++ 1장/아이템 3 코드 생성과 타입이 관계없음을 이해하기/check-property.ts
@@ -0,0 +1,16 @@
+interface Square {
+  width: number;
+}
+
+interface Rectangle extends Square {
+  height: number;
+}
+
+type Shape = Square | Rectangle;
+
+function calculateArea(shape: Shape) {
+  if ('height' in shape) {
+    return shape.width * shape.height;
+  }
+  return shape.width * shape.width;
+}
